214925c05eb922007c08fdd3ed8cce34eeca651e
Fatma Hameed <fatma78hameed@gmail.com>
2020-07-15 14:48:42 -0700

    finalizing exercise 4 js

---
+++ Week1/Homework/js-exercises/logString.js
@@ -0,0 +1,7 @@
+'use strict';
+let myString = 'Fatma Hameed Salah';
+console.log('The value of this string will be my full name Fatma Hameed Salah');
+console.log(myString);
+myString = 'Fatma';
+console.log('The value of this string will be Fatma');
+console.log(myString);
