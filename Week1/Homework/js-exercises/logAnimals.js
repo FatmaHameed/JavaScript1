474814bcf6ac489521a6396a10e12b96fc1b0d06
Fatma Hameed <fatma78hameed@gmail.com>
2020-07-15 16:06:41 -0700

    finalizing exercise 6 js

---
+++ Week1/Homework/js-exercises/logAnimals.js
@@ -0,0 +1,8 @@
+'use strict';
+let emptyItems = [];
+console.log('I think the value of this array is an empty array');
+console.log(emptyItems);
+let myFavoriteAnimals = ['Cats', 'Rabbits', 'Birds'];
+console.log(myFavoriteAnimals);
+myFavoriteAnimals.push('Piglet');
+console.log(myFavoriteAnimals);
