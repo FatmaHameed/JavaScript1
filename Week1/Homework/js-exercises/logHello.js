6b32d0208c5fa7af2691f1d21c05db052d9b505f
Fatma Hameed <fatma78hameed@gmail.com>
2020-07-15 13:28:37 -0700

    Finishing exercise 1 of js-exercises

---
+++ Week1/Homework/js-exercises/logHello.js
@@ -0,0 +1,10 @@
+console.log('Hello world!'); //English
+console.log('Bonjour le monde!'); //French
+console.log('Hallo Wereld!'); //Dutch
+console.log('Hola Mundo!'); //Spanish
+console.log('Привет мир!'); //Russian
+console.log('Selam Dünya!'); //Turkish
+console.log('नमस्ते दुनिया!'); //Hindi
+console.log('Hej Verden!'); //Danish
+console.log('Hallo Welt!'); //German
+console.log('你好，世界！'); //Chinese
