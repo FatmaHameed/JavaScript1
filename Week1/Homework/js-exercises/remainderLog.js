e95be21b01d615bd237995113b27ffac4adfb94f
Fatma Hameed <fatma78hameed@gmail.com>
2020-07-16 01:37:23 -0700

    finalizing exercise 9 js

---
+++ Week1/Homework/js-exercises/remainderLog.js
@@ -0,0 +1,20 @@
+'use strict';
+/*1- The value of x after the calculation will be 1.
+That is because x = 7; 7 / 3 = 2 and the remainder (x % 3) is 1.
+2- The value of y after calculation will also be 1.
+Because y = 21; 21/4 = 5 and the remainder = 1.
+3- The value of z after the calculation will be also 1.
+Because z = 13; 13/2 = 6 and the remainder = 1. */
+
+// let try it in JS
+let x = 7;
+x = x % 3;
+console.log(x); // The result will be 1.
+
+let y = 21;
+y = y % 4;
+console.log(y); // The result will be 1.
+
+let z = 13;
+z = z % 2;
+console.log(z); // The result will be 1.
