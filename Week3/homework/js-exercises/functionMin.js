45de7885a78120e333f5513deb3c57f392ad9b97
Fatma Hameed <fatma78hameed@gmail.com>
2020-07-28 09:52:02 -0700

    trying to solve coding in drinkTray

---
+++ Week3/homework/js-exercises/functionMin.js
@@ -0,0 +1,39 @@
+let numbers = [9, 4, 1, 7, 3, 8];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] < smallest) {
+//     smallest = numbers[i];
+//   }
+// }
+// function smallestNum(numbers) {
+//   return Math.min (numbers[]);
+// }
+// function smallestNum() {
+//   return math.min(numbers[]);
+// }
+// // console.log(smallest);
+// console.log(smallestNum());
+
+// function smallestNum(numbers) {
+//   let smallest = numbers[0];
+//   for (let i = 0; i < numbers.length; i++) {
+//       if (numbers[i] < smallest) {
+//         return  smallest = numbers[i];
+//        };
+// }
+
+// console.log (smallestNum(numbers))
+
+let number = [9, 4, 1, 7, 3, 8];
+// console.log(smallestNum(number));
+
+// function smallestNum(arr) {
+//   let temp = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < temp) {
+//       temp = arr[i];
+//     }
+//   }
+//   return temp;
+// }
+console.log(Math.min(...numbers));
--- homework/js-exercises/drinkTray.js
+++ homework/js-exercises/drinkTray.js
@@ -1,22 +1,44 @@
 'use strict';
 let drinkTray = [];
 const drinkTypes = [' cola', ' lemonade', ' water'];
+// for (let i = 0; i < 5; i++) {
+//   if ((i = 1)) {
+//     drinkTray.push(drinkTypes[0]);
+//   }
+//   if ((i = 2)) {
+//     drinkTray.push(drinkTypes[0]);
+//   }
+//   if ((i = 3)) {
+//     drinkTray.push(drinkTypes[1]);
+//   }
+//   if ((i = 4)) {
+//     drinkTray.push(drinkTypes[1]);
+//   }
+//   if ((i = 5)) {
+//     drinkTray.push(drinkTypes[2]);
+//   }
+// }
+
+// drinkTypes.splice(1, 0, 'cola', 'lemonade');
+// drinkTypes.splice(1, 0, 'cola ', 'lemonade ');
+
+// console.log(loopDrink);
+// drinkTray = drinkTypes.push('lemonade');
+
 for (let i = 0; i < 5; i++) {
-  if ((i = 1)) {
-    drinkTray.push(drinkTypes[0]);
-  }
-  if ((i = 2)) {
-    drinkTray.push(drinkTypes[0]);
-  }
-  if ((i = 3)) {
-    drinkTray.push(drinkTypes[1]);
-  }
-  if ((i = 4)) {
-    drinkTray.push(drinkTypes[1]);
-  }
-  if ((i = 5)) {
-    drinkTray.push(drinkTypes[2]);
+  // if (i < drinkTypes.length) {
+  //   drinkTray.push(drinkTypes[i]);
+  // }
+  // if (i < 5) {
+  //   drinkTray.push(drinkTypes[i - drinkTypes.length]);
+  // }
+  if (i > drinkTypes.length) {
+    drinkTray.push(drinkTypes[i]) + drinkTypes[i - drinkTypes.length];
   }
 }
+
+// // console.log(loopDrink);
+
 console.log(drinkTray);
+
 console.log('"' + 'Hey guys, I brought a ' + drinkTray + '!' + '"');
