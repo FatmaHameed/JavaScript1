d00e45ae651f6d28c686d7a1ac9eab10e791d17e
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-01 02:23:16 -0700

    added if statements to include all written functions and validate the credit number

--- Week3/homework/project-w3/creditCardValidation.js
+++ Week3/homework/project-w3/creditCardValidation.js
@@ -46,4 +46,31 @@ function validateCreditNumber(creditNumber) {
   function isNumeric(creditNumber) {
     return /^-{0,1}\d+$/.test(creditNumber);
   }
+
+  // write if statements to include all of above and validate the credit number
+  if (isNumeric(creditNumber) === false) {
+    return `Invalid! The input ${creditNumber} should contain only numbers!`;
+  }
+  if (digitCount(creditNumber) !== 16) {
+    return `Invalid! The input ${creditNumber} should be 16 characters!`;
+  }
+  if (checkIfGreater(creditNumber) === false) {
+    return `Invalid! The sum of the input ${creditNumber} should be more than 16`;
+  }
+  if (testSameDigit(creditNumber) === true) {
+    return `Invalid! The input ${creditNumber} should contain at least 2 different types of numbers!`;
+  }
+  if (checkEvenLastDigit(lastDigit) === false) {
+    return `Invalid! The final number of the input ${creditNumber} should be even!`;
+  }
+
+  if (
+    digitCount(creditNumber) === 16 &&
+    isNumeric(creditNumber) !== false &&
+    testSameDigit(creditNumber) !== true &&
+    checkEvenLastDigit(lastDigit) !== false &&
+    checkIfGreater(creditNumber) !== false
+  ) {
+    return `Success! The input ${creditNumber} is a valid credit card number!`;
+  }
 }
