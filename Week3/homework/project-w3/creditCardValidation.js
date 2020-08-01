bb4f549e641df41a322567de3c323c25dd72a9b1
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-01 01:17:06 -0700

    added the project directory and created a function of validateCreditNumber

---
+++ Week3/homework/project-w3/creditCardValidation.js
@@ -0,0 +1,11 @@
+'use strict';
+// start with the function to validate the credit number
+function validateCreditNumber(creditNumber) {}
+
+console.log(validateCreditNumber('6666666666661667'));
+console.log(validateCreditNumber('4444444444444444'));
+console.log(validateCreditNumber('a92332119c011112'));
+console.log(validateCreditNumber('666666666666166'));
+console.log(validateCreditNumber('0000000000000008'));
+console.log(validateCreditNumber('0000000000.00008'));
+console.log(validateCreditNumber('6666666666661666'));
