a70554358a4abcbfe9812181b3a7387540e734a3
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-01 02:28:34 -0700

    testing the function of validateCreditNumber

--- Week3/homework/project-w3/creditCardValidation.js
+++ Week3/homework/project-w3/creditCardValidation.js
@@ -74,3 +74,10 @@ function validateCreditNumber(creditNumber) {
     return `Success! The input ${creditNumber} is a valid credit card number!`;
   }
 }
+console.log(validateCreditNumber('6666666666661667'));
+console.log(validateCreditNumber('4444444444444444'));
+console.log(validateCreditNumber('a92332119c011112'));
+console.log(validateCreditNumber('666666666666166'));
+console.log(validateCreditNumber('0000000000000008'));
+console.log(validateCreditNumber('0000000000.00008'));
+console.log(validateCreditNumber('6666666666661666'));
