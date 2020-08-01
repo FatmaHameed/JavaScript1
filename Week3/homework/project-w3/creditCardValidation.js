8a69d3609701cbd9af67d73cb9d55e48ede4a826
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-01 02:16:57 -0700

    added the function of check out the sum of all the numbers must be greater than 16

--- Week3/homework/project-w3/creditCardValidation.js
+++ Week3/homework/project-w3/creditCardValidation.js
@@ -27,12 +27,18 @@ function validateCreditNumber(creditNumber) {
       return false;
     }
   }
-}
 
-console.log(validateCreditNumber('6666666666661667'));
-console.log(validateCreditNumber('4444444444444444'));
-console.log(validateCreditNumber('a92332119c011112'));
-console.log(validateCreditNumber('666666666666166'));
-console.log(validateCreditNumber('0000000000000008'));
-console.log(validateCreditNumber('0000000000.00008'));
-console.log(validateCreditNumber('6666666666661666'));
+  // Write a function to check out the sum of all the numbers must be greater than 16
+
+  let splitDigits = creditNumber.toString().split('').map(Number);
+  // console.log(splitDigits);
+  let sumDigit = splitDigits.reduce((a, b) => a + b);
+  // console.log(sumDigit);
+  function checkIfGreater(creditNumber) {
+    if (sumDigit <= 16) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
