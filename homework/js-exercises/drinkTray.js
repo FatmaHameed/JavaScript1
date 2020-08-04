ba1e843247fae3d275b8bc338e396ae99051020c
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-04 03:36:51 -0700

    modified the drinkTray.js file

--- homework/js-exercises/drinkTray.js
+++ homework/js-exercises/drinkTray.js
@@ -2,22 +2,24 @@
 let drinkTray = [];
 const drinkTypes = [' cola', ' lemonade', ' water'];
 
+//create a function to count the number occurrence of the drink in the drinkTypes
 const countDrinkType = function (arr, val) {
   return arr.reduce((acc, elem) => {
     return val === elem ? acc + 1 : acc;
   }, 0);
 };
 
-for (let i = 0; i < 5; i++) {
+const visitors = 5;
+const drinkPerVisitor = 2;
+//create loop inside loop to push the drinks to drinkTray
+for (let i = 0; i < visitors; i++) {
   for (let s = 0; s < drinkTypes.length; s++) {
-    if (countDrinkType(drinkTray, drinkTypes[0]) < 2) {
-      drinkTray.push(drinkTypes[0]);
-    } else if (countDrinkType(drinkTray, drinkTypes[1]) < 2) {
-      drinkTray.push(drinkTypes[1]);
-    } else if (countDrinkType(drinkTray, drinkTypes[2]) < 1) {
-      drinkTray.push(drinkTypes[2]);
+    if (countDrinkType(drinkTray, drinkTypes[s]) < drinkPerVisitor) {
+      drinkTray.push(drinkTypes[s]);
+      break;
     }
   }
 }
+// logging the final phrase
 
 console.log(`"Hey guys, I brought a${drinkTray}!" `);
